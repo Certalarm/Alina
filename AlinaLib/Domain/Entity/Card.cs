78899c8517cf241345c30287cb619923c297d051
Евгений <evgen_rusinov@mail.ru>
2024-01-29 12:55:54 +0800

    Создал входные и выходные Entity

---
+++ AlinaLib/Domain/Entity/Card.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlinaLib.Domain.Entity
+{
+    internal class Card
+    {
+        public readonly string UserId;
+        public readonly string Pan;
+        public readonly string ExpDate;
+
+
+        #region .ctors
+        public Card(string userId, string pan, string expDate)
+        {
+            UserId = userId;
+            Pan = pan;
+            ExpDate = expDate;
+        }
+        #endregion
+    }
+}
---
+++ AlinaLib/Domain/Entity/User.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlinaLib.Domain.Entity
+{
+    internal class User
+    {
+        public string UserId { get; }
+        public string Name { get; }
+        public string SecondName { get; }
+        public string Number { get; }
+
+
+        #region .ctors
+        public User(string userId, string name, string secondName, string number)
+        {
+            UserId = userId;
+            Name = name;
+            SecondName = secondName;
+            Number = number;
+        }
+        #endregion
+    }
+}
